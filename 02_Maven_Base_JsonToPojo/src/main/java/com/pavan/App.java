122549852111c7a67fd0f416d1309fc1e380e7f7
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 04:41:09 +0530

    Working with the maven basic project to learn the conversion from the json to java

--- 02_Maven_Base_JsonToPojo/src/main/java/com/pavan/App.java
+++ 02_Maven_Base_JsonToPojo/src/main/java/com/pavan/App.java
@@ -15,15 +15,42 @@ public class App
     public static void main( String[] args )
     {
     	
+		/*
+		 * try { // Create Object Mapper ObjectMapper mapper = new ObjectMapper();
+		 * 
+		 * // using that Object read the data from json file and convert into pojo
+		 * object Customer customer = mapper.readValue(new
+		 * File("data/sample-lite.json"), Customer.class); System.out.println(customer);
+		 * } catch (IOException e) { e.printStackTrace(); }
+		 */
+    	
+    	
     	try {
-    		// Create Object Mapper
-    		ObjectMapper mapper = new ObjectMapper();
-    		
-    		// using that Object read the data from json file and convert  into pojo object
-			Customer customer = mapper.readValue(new File("data/sample-lite.json"), Customer.class);
-			System.out.println(customer);
+			// Create Object Mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			// using that Object read the data from json file and convert into pojo object
+			Customer customer = mapper.readValue(new File("data/sample-full.json"), Customer.class);
+
+			// printing simple values
+			System.out.println("Id is :: " + customer.getId());
+			System.out.println("FirstName is :: " + customer.getFirstName());
+			System.out.println("LastName  is :: " + customer.getLastName());
+			System.out.println("IsActive     :: " + customer.isActive());
+
+			// Printing HAS-Property data
+			System.out.println("Customer Addres is :: " + customer.getAddress());
+
+			// Printing Languages[] information
+			for (String language : customer.getLanguages()) {
+				System.out.println(language);
+			}
+
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+    	
+    	
     }
 }
