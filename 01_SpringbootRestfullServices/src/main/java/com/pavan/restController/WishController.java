8c548f9e75222e2ba82e33409bc29b0abe9b5431
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 03:11:39 +0530

    first Project to See the Restfull services Working

--- 01_SpringbootRestfullServices/src/main/java/com/pavan/restController/WishController.java
+++ 01_SpringbootRestfullServices/src/main/java/com/pavan/restController/WishController.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/wish")
 public class WishController {
 	
+	@GetMapping(value = "/msg")
+	@ResponseBody
 	public ResponseEntity<String> generateWishMessage()
 	{
 		LocalDateTime ldt = LocalDateTime.now();
