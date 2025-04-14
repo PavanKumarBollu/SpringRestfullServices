62c746b490091aaf863c530351fc202cd75bf652
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 21:57:48 +0530

    Working with the RequestParam and PathVariables in Restfull Services

--- 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/RequestParamImpl.java
+++ 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/RequestParamImpl.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +13,7 @@ public class RequestParamImpl {
 	
 	
 	@GetMapping(value="/user")
-	public ResponseEntity<String> customer(Integer id, String name)
+	public ResponseEntity<String> customer( @RequestParam Integer id,  @RequestParam String name)
 	{
 		String body = "The Given Id : " +id+ " And Name Is : " + name;
 		return new ResponseEntity<String>(body, HttpStatus.OK);
