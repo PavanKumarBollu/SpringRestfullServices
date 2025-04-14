affb6c2e2112ebeecd90c5ee90333716b1078cd0
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 06:20:36 +0530

    Working with the maven basic project to learn the conversion from pojo to json

--- 02_Maven_Base_PojoToJson/src/main/java/com/pavan/App.java
+++ 02_Maven_Base_PojoToJson/src/main/java/com/pavan/App.java
@@ -1,10 +1,45 @@
 package com.pavan;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pavan.model.Product;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	try {
+    		// Create Object Mapper
+    		ObjectMapper mapper = new ObjectMapper();
+    		Product product = new Product();
+    		product = getObjectData(product);
+    		System.out.println("In java format :: " + product);
+    		
+    		// Converting java object into JsonString
+			String asString = mapper.writeValueAsString(product);
+			System.out.println("In json format :: " + asString);
+			
+			// Writing the data to json file
+						mapper.writeValue(new File("product-list.json"), product);
+						System.out.println("Wrote the data to json file");
+			
+			
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
     }
+    
+    public static Product getObjectData(Product product) {
+
+		product.setPid(10);
+		product.setPname("fossil");
+		product.setPrice(24556.0);
+		product.setTypes(new String[] { "chronography", "simpledial", "automatic" });
+
+		return product;
+	}
+    
 }
