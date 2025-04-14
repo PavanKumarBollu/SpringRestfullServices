8baf1fa567c7fbaf10f1fc887bb758f506221e4f
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 22:02:23 +0530

    Working with the RequestParam and PathVariables in Restfull Services and given default values to the request Param

--- 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/RequestParamImpl.java
+++ 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/RequestParamImpl.java
@@ -8,16 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/param")
+@RequestMapping(value = "/param")
 public class RequestParamImpl {
-	
-	
-	@GetMapping(value="/user")
-	public ResponseEntity<String> customer( @RequestParam(defaultValue = "10", required  = false) Integer id,  @RequestParam String name)
-	{
-		String body = "The Given Id : " +id+ " And Name Is : " + name;
+
+	@GetMapping(value = "/user")
+	public ResponseEntity<String> customer(@RequestParam(defaultValue = "10", required = false) Integer id,
+			@RequestParam(defaultValue = "Pavan", required = false) String name) {
+		String body = "The Given Id : " + id + " And Name Is : " + name;
 		return new ResponseEntity<String>(body, HttpStatus.OK);
 	}
-	
-	
+
 }
