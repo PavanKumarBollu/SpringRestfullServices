61100c5bea901f6a0d2e8b75a87ece83192f0a02
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 06:58:41 +0530

    Working with the springBoot project to learn the conversion from pojo to json

--- 01_SpringbootRestfullAutoResponseBodyWhenWorkingWithString/src/main/java/com/pavan/controller/WishController.java
+++ 01_SpringbootRestfullAutoResponseBodyWhenWorkingWithString/src/main/java/com/pavan/controller/WishController.java
@@ -1,5 +1,16 @@
 package com.pavan.controller;
 
-public class WishController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(value="/wish")
+public class WishController {
+	
+	@GetMapping(value="/msg")
+	public String wishUser()
+	{
+		return "Welcome to the Spring Restfull Services";
+	}
 }
