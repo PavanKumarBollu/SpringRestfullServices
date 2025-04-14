b3af5a4d4da557f88d6e8010009ab59e20b45d6f
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 21:46:54 +0530

    Working with the all kind of methods in restfull services:

--- 03_SpringbootRestfullDifferentTypesofMethod_POC/src/main/java/com/pavan/restcontroller/RestControllerImpl.java
+++ 03_SpringbootRestfullDifferentTypesofMethod_POC/src/main/java/com/pavan/restcontroller/RestControllerImpl.java
@@ -2,6 +2,7 @@ package com.pavan.restcontroller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +37,12 @@ public class RestControllerImpl {
 	public ResponseEntity<String> updateCustomer()
 	{
 		return new ResponseEntity<String>("Put-Method of the customer", HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value="/delete")
+	public ResponseEntity<String> deleteCustomer()
+	{
+		return new ResponseEntity<String>("Delete-method of the Customer", HttpStatus.OK);
 	}
 
 }
