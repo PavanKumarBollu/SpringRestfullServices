eda86cc1ec99282d00dd8dbf1cbbd0eef9274365
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 03:12:52 +0530

    Workinn With the Ticket Managment App and using all the Restfull services testing with the PostMan tool

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/exception/TouristNotFoundException.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/exception/TouristNotFoundException.java
@@ -2,9 +2,13 @@ package com.pavan.exception;
 
 public class TouristNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	public TouristNotFoundException(String msg) {
+		super(msg);
+	}
+
+	
+	
+	
 }
