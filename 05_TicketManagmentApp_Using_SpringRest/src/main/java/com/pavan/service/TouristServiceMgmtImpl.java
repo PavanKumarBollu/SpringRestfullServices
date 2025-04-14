c0660d62609a977cd8f74198d7927f67d6a20853
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 03:17:12 +0530

    Workinn With the Ticket Managment App and using all the Restfull services testing with the PostMan tool

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/service/TouristServiceMgmtImpl.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/service/TouristServiceMgmtImpl.java
@@ -1,8 +1,48 @@
 package com.pavan.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.pavan.model.Tourist;
 
 @Service
 public class TouristServiceMgmtImpl implements ITouristMgmtService {
+
+	@Override
+	public String RegisterTourist(Tourist tourist) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Tourist> findAllTourist() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tourist fetchTouristById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateTouristByDetails(Tourist t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String updateTouristById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String DeleteTouristById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
