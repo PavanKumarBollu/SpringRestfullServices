2310725f626d99c28538cf541e9c0e454241b831
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 05:04:30 +0530

    Workinn With the Ticket Managment App and using all the Restfull services testing with the PostMan tool and Included the Advices Of AOP and final Version of the Ticket Managment App

--- 06_TicketManagmentApp_Using_RestControllerAdvice/src/main/java/com/pavan/advice/TouristErrorControllerAdvice.java
+++ 06_TicketManagmentApp_Using_RestControllerAdvice/src/main/java/com/pavan/advice/TouristErrorControllerAdvice.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.pavan.error.ErrorDetails;
@@ -12,11 +13,15 @@ import com.pavan.exception.TouristNotFoundException;
 @RestControllerAdvice
 public class TouristErrorControllerAdvice {
 
+	@ExceptionHandler(TouristNotFoundException.class)
 	public ResponseEntity<ErrorDetails> handleTouristNotFoundException(TouristNotFoundException te) {
 		System.out.println("TouristErrorControllerAdvice.handleTouristNotFoundException()");
 
 		return new ResponseEntity<ErrorDetails>(new ErrorDetails(LocalDateTime.now(), te.getMessage(), "404-NotFound"),
 				HttpStatus.NOT_FOUND);
 	}
+	
+	
+	
 
 }
