3900c0338218e29703491192d6c607e8d3f21743
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 03:14:53 +0530

    Workinn With the Ticket Managment App and using all the Restfull services testing with the PostMan tool

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/dao/ITouristRepo.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/dao/ITouristRepo.java
@@ -1,9 +1,14 @@
 package com.pavan.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.pavan.model.Tourist;
 
 public interface ITouristRepo extends CrudRepository<Tourist, Integer> {
-
+	
+	public String RegisterTourist(Tourist tourist);
+	public List<Tourist> findAllTourist();
+	
 }
