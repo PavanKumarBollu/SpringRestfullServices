41e5bb36b689ad07345ed3db7af87f5fbd369bfe
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 22:09:33 +0530

    Working with the PathVariables in Restfull Services

--- 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/PathVariableImpl.java
+++ 04_SpringbootRestfullRequestParamAndPathVariable/src/main/java/com/pavan/restcontroller/PathVariableImpl.java
@@ -3,18 +3,18 @@ package com.pavan.restcontroller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value="/path")
+@RequestMapping(value = "/path")
 public class PathVariableImpl {
 
-	
-	@GetMapping(value="/emp/{id}/{name}")
-	public ResponseEntity<String> employee(Integer id, String name)
-	{
-		String body = "Id Of the Employee : " + id + " and Name Of the Employee : " + name
+	@GetMapping(value = "/emp/{eId}/{eName}")
+	public ResponseEntity<String> employee( @PathVariable(value="eId") Integer id, @PathVariable(value="eName") String name) {
+		String body = "Id Of the Employee : " + id + " and Name Of the Employee : " + name;
+		// Logic with the collected values
 		return new ResponseEntity<String>(body, HttpStatus.OK);
 	}
 }
