05abb521dff72873c90aa7a399794aaef3b3fc2c
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 04:37:29 +0530

    Working with the RequestParam and PathVariables in Restfull Services and given default values to the request Param

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
@@ -16,16 +16,14 @@ import com.pavan.model.Tourist;
 import com.pavan.service.ITouristMgmtService;
 
 @RestController
-@RequestMapping(value="/api/tourist")
+@RequestMapping(value = "/api/tourist")
 public class TouristController {
 
 	@Autowired
 	private ITouristMgmtService service;
-	
-	
-	@PostMapping(value="/register")
-	public ResponseEntity<String> enrollTourist( @RequestBody Tourist tourist)
-	{
+
+	@PostMapping(value = "/register")
+	public ResponseEntity<String> enrollTourist(@RequestBody Tourist tourist) {
 		try {
 			String result = service.RegisterTourist(tourist);
 			return new ResponseEntity<String>(result, HttpStatus.OK);
@@ -33,32 +31,35 @@ public class TouristController {
 			return new ResponseEntity<String>("Problem in tourist enrollment", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@GetMapping(value="/findAll")
-	public ResponseEntity<?> displayAllTourists()
-	{
+
+	@GetMapping(value = "/findAll")
+	public ResponseEntity<?> displayAllTourists() {
 		try {
 			List<Tourist> list = service.findAllTourist();
 			return new ResponseEntity<List<Tourist>>(list, HttpStatus.OK);
 		} catch (Exception e) {
-			
+
 			return new ResponseEntity<String>("Problem in fetching tourist", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@GetMapping(value="/find/{tId}")
-	public ResponseEntity<?> findTouristById( @PathVariable(name = "tId") Integer id)
-	{
+
+	@GetMapping(value = "/find/{tId}")
+	public ResponseEntity<?> findTouristById(@PathVariable(name = "tId") Integer id) {
 		try {
 			Tourist tourist = service.fetchTouristById(id);
-			return new ResponseEntity<Tourist> (tourist,HttpStatus.OK);
+			return new ResponseEntity<Tourist>(tourist, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	
-	
-	
-	
+
+	public ResponseEntity<String> modifyTourist(Tourist tourist) {
+		try {
+			String touristByDetails = service.updateTouristByDetails(tourist);
+			return new ResponseEntity<String>(touristByDetails, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+	}
+
 }
