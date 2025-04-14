62aaa6b04aae41ce518d0062edd4b672b7cb22e8
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 04:40:21 +0530

    Working with the RequestParam and PathVariables in Restfull Services and given default values to the request Param

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -63,5 +64,14 @@ public class TouristController {
 			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
 		}
 	}
+	
+	@PatchMapping(value="/modifyBudget/{tId}/{hike}")
+	public ResponseEntity<String> modifyTouristBudgetById(Integer id, Float hikeAmt)
+	{
+		
+	}
+	 
+	
+	
 
 }
