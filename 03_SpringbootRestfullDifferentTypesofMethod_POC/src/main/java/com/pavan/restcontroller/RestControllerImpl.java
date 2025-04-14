10ed5f6444b9e31f34d132fa939f4e74276a1cf4
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 21:40:53 +0530

    Working with the all kind of methods in restfull services:

--- 03_SpringbootRestfullDifferentTypesofMethod_POC/src/main/java/com/pavan/restcontroller/RestControllerImpl.java
+++ 03_SpringbootRestfullDifferentTypesofMethod_POC/src/main/java/com/pavan/restcontroller/RestControllerImpl.java
@@ -1,6 +1,7 @@
 package com.pavan.restcontroller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,12 @@ public class RestControllerImpl {
 	public ResponseEntity<String> showCustomer()
 	{
 		return new ResponseEntity<String>("Get-Method of the Customer", HttpStatus.OK);
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<String> registerCustomer()
+	{
+		return new ResponseEntity<String>("POST-method of the Customer", HttpStatus.OK);
 	}
 
 }
