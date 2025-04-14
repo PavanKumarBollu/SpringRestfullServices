1a38d19b2c8b6d553c3557acb1fc01dbf1c53d68
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 06:30:47 +0530

    Working with the springBoot project to learn the conversion from pojo to json

--- 02_SpringbootWorkingWithJson/src/main/java/com/pavan/model/Company.java
+++ 02_SpringbootWorkingWithJson/src/main/java/com/pavan/model/Company.java
@@ -1,5 +1,12 @@
 package com.pavan.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Company {
 	private String cname;
 	private String ctype;
