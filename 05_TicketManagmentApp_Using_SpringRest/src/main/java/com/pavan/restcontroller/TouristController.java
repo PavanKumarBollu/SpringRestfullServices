ad269f5dd0d7b965809f623a8b8015fafb13331a
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-15 03:40:40 +0530

    Workinn With the Ticket Managment App and using all the Restfull services testing with the PostMan tool

--- 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
+++ 05_TicketManagmentApp_Using_SpringRest/src/main/java/com/pavan/restcontroller/TouristController.java
@@ -3,6 +3,7 @@ package com.pavan.restcontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,7 +31,7 @@ public class TouristController {
 		}
 	}
 	
-	
+	@GetMapping(value="/findAll")
 	public ResponseEntity<?> displayAllTourists()
 	{
 		return null;
