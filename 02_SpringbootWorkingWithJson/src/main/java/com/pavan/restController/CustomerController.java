ee891d6f74df96a954c42f7da60b2594091f92f2
PavanKumarB <bollupavankumar12@gmail.com>
2025-04-14 06:34:18 +0530

    Working with the springBoot project to learn the conversion from pojo to json

--- 02_SpringbootWorkingWithJson/src/main/java/com/pavan/restController/CustomerController.java
+++ 02_SpringbootWorkingWithJson/src/main/java/com/pavan/restController/CustomerController.java
@@ -1,17 +1,35 @@
 package com.pavan.restController;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pavan.model.Company;
 import com.pavan.model.Customer;
 
 @RestController
 @RequestMapping(value="/api/customer")
 public class CustomerController {
 	
-	public ResponseEntity<Customer> showAllCustomer()
+	public ResponseEntity<Customer> showAllCustomer(@PathVariable Integer id)
 	{
+		// get from database
+		System.out.println("Customer data for the id :: " + id);
+		Customer customer = new Customer();
+		customer.setCno(id);
+		customer.setCname("sachin");
+		customer.setBillAmount(54.5f);
+		customer.setTeamNames(new String[] { "IND", "MI", "AsiaXI", "Mumbai" });
+		customer.setStudies(List.of("10th", "12th", "Engineering"));
+		customer.setPhoneNumbers(Set.of(9994445556L, 994349845L, 98765678L));
+		customer.setIdDetails(Map.of("adhar", 99453123432L, "panNo", "DOOPQRCL12"));
+		customer.setCompany(new Company("MI", "IPL", "Mumbai", 45));
+		
 		return entity;
 	}
 
